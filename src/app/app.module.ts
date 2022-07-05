a8cb762606bc881fd9cc9e262f5fe878da9d95ed
Yahoshuva <pasupuletiyahoshuva1999@gmail.com>
2022-07-05 11:55:39 -0700

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CourseComponent } from './course/course.component';
+import { AboutComponent } from './about/about.component';
+import { JoinnowComponent } from './joinnow/joinnow.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CourseComponent,
+    AboutComponent,
+    JoinnowComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
